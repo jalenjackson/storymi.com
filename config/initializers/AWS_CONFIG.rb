c03991037a9eaf7f8dd47264f9eb5a3be44982f2
Jalen Jackson <jalenjack201@hotmail.com>
2017-05-03 22:25:15 -0400

    wed

---
+++ config/initializers/AWS_CONFIG.rb
@@ -0,0 +1,7 @@
+AWS_CONFIG = {
+    'access_key_id' => 'AKIAJDATCTI6UROYWHJQ',
+    'secret_access_key' => '$1I9ACVi7sKvFkHYGqo4zTt1gDpMPtd0/ekGRjITx',
+    'bucket' => 'storybucketstorage',
+    'acl' => 'public-read',
+    'key_start' => 'uploads/'
+}
