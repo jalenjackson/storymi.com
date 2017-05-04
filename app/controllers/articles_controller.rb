dc4e0d34ce7a39d0ed9d00472b36827805f46435
Jalen Jackson <jalenjack201@hotmail.com>
2017-05-04 19:47:46 -0400

    fr

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.paginate(:page => params[:page], :per_page => 30)
   end
 
   # GET /articles/1
