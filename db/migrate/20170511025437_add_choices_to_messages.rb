9decaa1b18c8c5f515dcc1c76f2999d791691358
Jalen Jackson <jalenjack201@hotmail.com>
2017-05-10 23:49:02 -0400

    new commit

--- db/migrate/20170511025437_add_choices_to_messages.rb
+++
@@ -1,6 +0,0 @@
-class AddChoicesToMessages < ActiveRecord::Migration[5.0]
-  def change
-    add_column :messages, :first_choice, :string
-    add_column :messages, :second_choice, :string
-  end
-end
