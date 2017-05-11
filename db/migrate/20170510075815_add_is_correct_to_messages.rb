5ac2bfe696509070c36b5eadc2e2be30716ffa33
Jalen Jackson <jalenjack201@hotmail.com>
2017-05-10 22:27:10 -0400

    dbase

--- db/migrate/20170510075815_add_is_correct_to_messages.rb
+++ db/migrate/20170510075815_add_is_correct_to_messages.rb
@@ -1,5 +1,9 @@
 class AddIsCorrectToMessages < ActiveRecord::Migration[5.0]
   def change
+    if column_exists? :messages, :is_correct
+      remove_column :messages, :is_correct
+    end
+
     add_column :messages, :is_correct, :boolean
   end
 end
--- db/migrate/20170510080146_add_new_choice_to_messages.rb
+++ db/migrate/20170510080146_add_new_choice_to_messages.rb
@@ -1,5 +1,9 @@
 class AddNewChoiceToMessages < ActiveRecord::Migration[5.0]
   def change
+    if column_exists? :messages, :is_correct2
+      remove_column :messages, :is_correct2
+    end
+
     add_column :messages, :is_correct2 , :boolean
   end
 end
